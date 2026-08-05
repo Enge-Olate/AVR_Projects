1ebe76221c075354537651b12c1592980b775037
Enge-Olate <marcioalexisolate@live.com>
2026-08-04 22:15:05 -0300

    Implementando cabecalho para botão. Lógica para estado do botão se botão HIGH LED_BUILTIN LOW, senão LED_BUILTIN on.

--- src/main.c
+++ src/main.c
@@ -1,15 +1,20 @@
 #include <util/delay.h>
-#include<avr/io.h>
+#include <avr/io.h>
 #include "gpio.h"
+#include "button.h"
 
 int main(void)
 {
+    button_init();
     gpio_led_init();
     while (1)
     {
-        gpio_led_toggle();
-        _delay_ms(500);
+        if (button_read() == BUTTON_PRESSED)
+        {
+            gpio_led_toggle();
+            _delay_ms(500);
+        }
     }
-    
+
     return 0;
 }
