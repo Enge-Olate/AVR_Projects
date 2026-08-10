98f73cc3769ccc88dad6c0d737fff4d39a760e04
Enge-Olate <marcioalexisolate@live.com>
2026-08-10 19:40:12 -0300

    Endereçamento de array para bases dos transistores.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include <util/delay.h>
-#include <avr/io.h>
 #include "ports.h"
 
 static const gpio_t LED_BUILTIN = {
@@ -9,8 +8,20 @@ static const gpio_t LED_BUILTIN = {
     .bit = PB5
 };
 
+static const gpio_t BASES[] = {
+    {&PORTD, &DDRD, &PIND, PD2},
+    {&PORTD, &DDRD, &PIND, PD3},
+    {&PORTD, &DDRD, &PIND, PD4},
+    {&PORTD, &DDRD, &PIND, PD5},
+};
+
 int main(void)
 {
+    gpio_output(&BASES[0]);
+    gpio_set(&BASES[0]);
+    
+    
+    
     gpio_output(&LED_BUILTIN);
     gpio_clear(&LED_BUILTIN);
     for(;;){
