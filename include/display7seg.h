5e1bbcd8a2e47b939d76dc97b21d5dd7b446aab9
Enge-Olate <marcioalexisolate@live.com>
2026-08-14 09:32:59 -0300

    Implementação para o display de sete segmentos.

---
+++ include/display7seg.h
@@ -0,0 +1,14 @@
+#ifndef DISPLAY7SET_H
+#define DISPLAY7SET_H
+
+#include <avr/io.h>
+#include "hc595.h"
+/* 
+* @brief Protótipo das funções para o display.
+ */
+
+ void display_init(const hc595_t *shift_reg, const gpio_t bases[4]);
+ void display_update_buffer(uint16_t value);
+ void display_multiplex_step(void);
+
+#endif/* DISPLAY7SET_H */
---
+++ src/display7seg.c
@@ -0,0 +1,1 @@
+#include "display7seg.h"
