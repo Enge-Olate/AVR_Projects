d5bfdaa63fc148ed18b8b79a04052c399f24efe3
Enge-Olate <marcioalexisolate@live.com>
2026-08-14 08:10:26 -0300

    Implementando timer1.

---
+++ include/timer1.h
@@ -0,0 +1,10 @@
+#ifndef TIMER1_H
+#define TIMER1_H
+
+#include <avr/io.h>
+
+/* @brief Protótipo de função para configurar o timer1 do chip ATmega328p. */
+
+void timer1_init_ctc(void);
+
+#endif /* TIMER1_H */
---
+++ src/timer1.c
@@ -0,0 +1,17 @@
+#include "timer1.h"
+
+/*
+ * @brief Implementação da função do timer 1.
+ *  Fórmula para calcular o tempo no modo CTC é:
+ *  TEMPO = PRESCALER * (1 + OCR1A) / FREQUÊNCIA CPU.
+ */
+
+void timer1_init_ctc(void)
+{
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCNT1 = 0;
+    OCR1A = 499;
+    TCCR1B |= (1 << WGM12) | (1 << CS11) | (1<<CS10);
+    TIMSK1 |= (1 << OCIE1A);
+}
