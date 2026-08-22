b86e81e79d9246f9d2b4dd2c4149ecb1569f6d02
Enge-Olate <marcioalexisolate@live.com>
2026-08-22 18:35:55 -0300

    Setando display para contar até 9999.

--- main.c
+++ main.c
@@ -1,23 +1,34 @@
+#include <avr/interrupt.h>
 #include <util/delay.h>
-#include "ports.h"
 #include "gpio_config.h"
-
+#include "display7seg.h"
+#include "timer1.h"
 #define BUTTON &BUTTON_STATE
+
+const hc595_t CI_PINS = {
+    .data = {&PORTB, &DDRB, &PINB, PB0},
+    .clock = {&PORTB, &DDRB, &PINB, PB1},
+    .latch = {&PORTB, &DDRB, &PINB, PB2},
+};
+
+ISR(TIMER1_COMPA_vect){
+    display_multiplex_step();
+}
+
 int main(void)
 {
-    gpio_output(&BASES[0]);
-    gpio_set(&BASES[0]);
-    gpio_input(BUTTON);
-
     gpio_output(&LED_BUILTIN);
     gpio_clear(&LED_BUILTIN);
-    while (1)
+    display_init(&CI_PINS, BASES);      
+    display_update_buffer(9999);
+    timer1_init_ctc();
+    uint16_t counter = 0;
+    while (1 && counter <= 9999)
     {
-        if (gpio_read(BUTTON))
-        {
-            gpio_toggle(&LED_BUILTIN);
-            _delay_ms(200);
-        }
+        counter ++;
+        display_update_buffer(counter);
+        gpio_toggle(&LED_BUILTIN);
+        _delay_ms(1000);
     }
 
     return 0;
--- src/display7seg.c
+++ src/display7seg.c
@@ -13,7 +13,7 @@ static volatile uint8_t digito_atual = 0;
  * @brief Mapeamento dos bytes para display de sete segmentos de catodo comum.
  */
 
-static const uint8_t BYTES[10] = {
+static const uint8_t BYTES[] = {
     0x3f, // 0
     0x06, // 1
     0x5b, // 2
@@ -24,6 +24,7 @@ static const uint8_t BYTES[10] = {
     0x07, // 7
     0x7f, // 8
     0x6f, // 9
+    0x80, // .
 };
 
 /*
